875c0e86c5ccf5161ba0730e3713f99eb46b4676
sanchezedgar <edgar.sanchez@medialets.com>
2013-10-04 11:59:31 -0400

    Updated cache headers and twitter.class.php location and name.

--- lib/twitter.php
+++ lib/twitter.php
@@ -1,15 +1,19 @@
 <?php
-// error_reporting(E_ALL);
-// ini_set('display_errors', '1');
 
-require('twitter.class.php');
+$expire = (isset($_GET['expire']) && is_numeric($_GET['expire']))? $_GET['expire'] : 300;
+header("Cache-Control: must-revalidate, public, max-age=".$expire.", s-maxage=".$expire);
+// header("Cache-Control: public");
+// header("Expires: " . gmdate("D, d M Y H:i:s", time() + $expire) . " GMT");
+header("Content-Type: application/javascript");
+
+require('lib/Twitter.class.php');
 
 $consumer_key = (isset($_GET['consumer_key']))? $_GET['consumer_key'] : 'LGqv8YPBjgIQFwu7YZ9IFw';
 $consumer_secret = (isset($_GET['consumer_secret']))? $_GET['consumer_secret'] : 'yRKwre0DMJVeCNrmUZUeAR0NqwaiO9ZvE0SiglCYWPY';
 $action = $_GET['action'];
 $param = (isset($_GET['param']))? $_GET['param'] : '';
 $expire = (isset($_GET['expire']) && is_numeric($_GET['expire']))? $_GET['expire'] : 300;
-$mmcb = (isset($_GET['mmcb']))? $_GET['mmcb'] : 'callback';
+$callback = 'medialets.twitter.jsonp.'.$_GET['callback'];
 
 $twitter = new Twitter($consumer_key, $consumer_secret, $expire);
 
@@ -20,9 +24,9 @@ if(isset($action) && method_exists($twitter,$action)){
         $results["query"] = $_SERVER['QUERY_STRING'];
         $results = json_encode($results);
     }
-    print $mmcb.'('. $results .')';
+    print $callback.'('. $results .')';
 } else {
-    $results = $mmcb.'({';
+    $results = $callback.'({';
     if($_GET['debug'] == 'true'){
         $results += '"query":"'.$_SERVER['QUERY_STRING'].'",';
     }
